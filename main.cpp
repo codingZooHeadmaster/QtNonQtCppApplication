b75898ddac187a35e1f76eebb71a75041daac161
Alain Redlinger <alain.redlinger@umontpellier.fr>
2019-02-16 17:33:45 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Une application C++ non Qt
+ * Tout est fait à la création du projet :
+ * New File or Project / Projects / Non-Qt Project / Plain C++ Application.
+ * Le terminal apparait au démarrage du debug.
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    cout << "x = ";
+    int x = 0;
+    cin >> x;
+
+    cout << "\nx = " << x << endl;
+    return 0;
+}
